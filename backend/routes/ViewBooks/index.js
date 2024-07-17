23ea2b5b089088e400b1aba76f52f6290f96e832
SoumabhaSaha15 <soumabhasaha1509+github@gmail.com>
2024-07-17 16:16:59 +0530

    book view & zod

--- backend/routes/ViewBooks/index.js
+++ backend/routes/ViewBooks/index.js
@@ -1,9 +1,11 @@
 import express from "express"
 import GetEndPoint from "./GET/endPoint.js";
+import covers from "./GET/covers.js";
 export default {
   routeName:"view-book",
   GET:{
-    endPoint:GetEndPoint
+    endPoint:GetEndPoint,
+    covers:covers
   },
   POST:{},
   PUT:{},
