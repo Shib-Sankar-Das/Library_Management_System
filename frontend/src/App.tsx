1b64124d91ba12d1c88f7610375d688be2e5286d
Shib Sankar Das <shibsankardas721@gmail.com>
2024-07-20 15:52:51 +0530

    Home Page created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import "./App.css";
 import { Routes, Route } from 'react-router-dom';
 import AllBooks from "./Pages/AllBooks";
 import CleentAuth from "./Pages/ClientAuthentication";
+import HomePage from "./Pages/HomePage";
 const App: React.FC = () => {
     return (
       <Routes>
         <Route path="/" element={<CleentAuth />} />
         <Route path="/client-auth" element={<CleentAuth />} />
         <Route path="/all-books" element={<AllBooks />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     );
 };
