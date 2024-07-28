121c521540e1406ba827d4c474aabab5f8823a18
SoumabhaSaha15 <soumabhasaha1509+github@gmail.com>
2024-07-28 14:16:50 +0530

    book component final

--- frontend/src/Components/Book.tsx
+++ frontend/src/Components/Book.tsx
@@ -1,43 +1,71 @@
 import React from "react";
 import { z } from "zod";
 import { bookCopySchema } from "../Validator/BookCopy";
+import styled from 'styled-components';
 type props = z.infer<typeof bookCopySchema>;
 const Book: React.FC<props> = (data: props) => {
+  React.useEffect(()=>{console.log(data)},[]);
+  const BookContainer = styled.div`
+  position: relative;
+  border-radius: 10px;
+  width: 280px;
+  height: 250px;
+  background-color: whitesmoke;
+  box-shadow: 1px 1px 12px #000;
+  transform-style: preserve-3d;
+  perspective: 2000px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #000;
+`;
+
+const Cover = styled.div`
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: lightgray;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: all 0.5s;
+  transform-origin: 0;
+  box-shadow: 1px 1px 12px #000;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+  &:hover {
+    transform: rotateY(-90deg);
+  }
+`;
+
+const Text = styled.p`
+  font-size: 20px;
+  font-weight: bolder;
+`;
+
   return (
     <>
-      <div className="max-w-[280px] max-h-[200px] min-h-[200px] overflow-hidden rounded-xl hover:scale-[90%]" data-name={data.Name} data-author={data.Author} data-publisher={data.Publisher} data-subject={data.Subject} data-isbn={data.ISBN} data-publishing-date={data.PublishingDate} >
-        <a href="#" className="group relative block bg-black min-h-[inherit]">
-          <img
-            alt=""
-            src={data.ImageLink}
-            className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
-            onError={(err) => {
-              err.currentTarget.src = "./fall_back_cover.jpg";
-            }}
-            style={{filter:'brightness(1.5)'}}
-          />
-
-          <div className="relative p-1 sm:p-2 lg:p-2">
-            <p className="text-2xl font-bold uppercase overflow-hidden text-ellipsis  tracking-widest text-white" style={{textShadow:'4px 4px 1px rgba(0,0,0, 0.9)'}} >
-              {data.Name}
-            </p>
-
-            <p className="text-base font-bold text-green-600 sm:text-base">
-              Author:{data.Author}
-            </p>
-
-            <div className="mt-5 sm:mt-2 lg:mt-10">
-              <div className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
-                <p className="text-sm text-white overflow-hidden max-h-[20px] overflow-ellipsis">
-                  Published by: {data.Publisher.slice(0,20)}
-                </p>
-                <p className="text-sm text-white overflow-hidden overflow-ellipsis">Subject: {data.Subject}</p>
-                <p className="text-sm text-white">ISBN: {data.ISBN}</p>
-              </div>
-            </div>
-          </div>
-        </a>
-      </div>
+      
+    <BookContainer>
+        <Text style={{textAlign:'left',paddingLeft:"5px"}}>
+          {"Author: "+data.Author}
+          {<br/>}
+          {"Publisher: "+data.Publisher}
+          <br/>
+          {"Subject: "+data.Subject}
+        </Text>
+      <Cover 
+        style={{
+          backgroundImage:`url('${data.ImageLink}')`,
+          backgroundPosition:"center center",
+          backgroundRepeat:'no-repeat',
+          backgroundSize:'cover'
+        }}>
+        <Text style={{backdropFilter:'blur(5px)'}}>{data.Name}</Text>
+      </Cover>
+    </BookContainer>
     </>
   );
 };
--- frontend/src/Pages/BooksView.tsx
+++ frontend/src/Pages/BooksView.tsx
@@ -2,15 +2,13 @@ import React from "react";
 import { z } from "zod";
 import { BookCopyModel } from "../Validator/BookCopy";
 import Book from "../Components/Book";
-// import SearchBar from "../Components/SearchBar";
 const BooksView : React.FC = () =>{
+  const SELECT_OPTION = ["Name","Author","ISBN","Publisher","Subject"];
   const [Data, SetData] = React.useState<z.infer<typeof BookCopyModel>>([]);
-  // let Options = [''];
   React.useEffect(()=>{
     fetch("api/books")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         const DATA = BookCopyModel.safeParse(data);
         (DATA.success)?SetData(()=>{
           return DATA.data;
@@ -32,18 +30,13 @@ const BooksView : React.FC = () =>{
           placeholder="Enter to Search..."
           className="w-full p-2 rounded-lg bg-gray-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
-        <select 
+        <select defaultValue={"Name"}
         className="ml-2 p-2 rounded-lg bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500">
-          {Object.keys(Data[0]).filter(item=>(item!="_id" && item!="Copies" && item!="PublishingDate" && item!="ImageLink")).map((item,index)=>(<option value={item} selected={(index==0)?true:false}>{item}</option>))}
-          
-          {/* <option value="option1">Author</option>
-          <option value="option2">Publisher</option>
-          <option value="option3">Subject</option>
-          <option value="option4">ISBN</option> */}
+          {SELECT_OPTION.map((item)=>(<option value={item} key={crypto.randomUUID()} >{item}</option>))}
         </select>
       </div>
     </div>
-    <div className="grid grid-flow-row grid-cols-[repeat(auto-fill,300px)] auto-rows-[240px] min-w-full m-0 p-2 justify-center align-middle gap-1.5 overflow-y-auto max-h-[90dvh]">
+    <div className="grid grid-flow-row grid-cols-[repeat(auto-fill,300px)] auto-rows-[280px] min-w-full m-0 p-2 justify-center align-middle gap-1.5 overflow-y-auto max-h-[90dvh]">
       {Data.map((item) => (<Book key={item._id} {...item} />))}
     </div>
     </>
